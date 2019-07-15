ae6800091dea8e1d9d3fd9ce4e1e8138e5a0d9f0
ruralkan <ruralkan@gmail.com>
2019-07-15 11:30:12 -0500

    Boilerplate of developing environment with babel, eslint and jest and tunnel

--- buildScripts/app.js
+++ buildScripts/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log(`Example app listening on port ${process.env.PORT}!`);
 });
